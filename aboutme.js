a0007aa03be61add9652e2d896630a47e8bbe051
staals19 <staals19@polyprep.org>
2017-08-01 11:47:29 -0400

    Create aboutme.js

---
+++ aboutme.js
@@ -0,0 +1,11 @@
+
+
+function isClicked() {
+  alert("button was clicked!");
+}
+
+var background_images = [colors1.jpg, colors2.jpg, colors3.jpg];
+
+function changeBg() {
+  document.body.style.backgroundImage = "colors.jpg";
+}
